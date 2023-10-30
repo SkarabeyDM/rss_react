c22f6bb6d7dae44c28058ab6718de7aa7774e0aa
SkarabeyDM <mechanoid0415@gmail.com>
2023-10-30 12:44:09 +0300

    feat: add `MainPage` component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Component } from 'react';
 import './App.scss';
+import { MainPage } from './pages/MainPage';
 
 export default class App extends Component {
   render() {
     return (
       <>
+        <MainPage></MainPage>
       </>
     );
   }
