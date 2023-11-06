5666eb5a5e106d8249a1f7410814ab72734b99f6
SkarabeyDM <mechanoid0415@gmail.com>
2023-11-06 15:01:11 +0300

    refactor: replace `MainPage` by `Catalog`

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { Component } from 'react';
 import './App.scss';
-import { MainPage } from './pages/MainPage';
+import { Catalog } from './widgets/Catalog';
 
 export default class App extends Component {
   render() {
     return (
       <>
-        <MainPage></MainPage>
+        <Catalog />
       </>
     );
   }
