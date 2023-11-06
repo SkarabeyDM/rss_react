2298f7e0f575fe220489a89c099ec7738d8d97c1
SkarabeyDM <mechanoid0415@gmail.com>
2023-11-06 14:53:45 +0300

    chore: fix api visibility

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '',
-});
+
+export default ({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {
+    POKEMONTCG_API_KEY: process.env.VITE_API_KEY,
+    ...loadEnv(mode, process.cwd()),
+  };
+
+  console.log(process.env.POKEMONTCG_API_KEY);
+
+  return defineConfig({
+    plugins: [react()],
+    base: '',
+    define: {
+      'process.env': process.env,
+    },
+  });
+};
