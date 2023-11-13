75b8686993b9bc188c71d478658a10d14ebc481a
SkarabeyDM <mechanoid0415@gmail.com>
2023-11-13 10:19:50 +0300

    chore: set up `vitest`

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -13,6 +16,12 @@ export default ({ mode }) => {
   return defineConfig({
     plugins: [react()],
     base: '/rss_react/',
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/setupTests.ts'],
+      coverage: { all: true },
+    },
     define: {
       'process.env': process.env,
     },
