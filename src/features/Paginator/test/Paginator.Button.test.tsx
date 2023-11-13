a1de45c669db54edbacf2917c75ba719cf107627
SkarabeyDM <mechanoid0415@gmail.com>
2023-11-13 10:22:35 +0300

    test: add `PaginatorButton` test cases

---
+++ src/features/Paginator/test/Paginator.Button.test.tsx
@@ -0,0 +1,24 @@
+import { describe, expect, test } from 'vitest';
+import { PaginatorButton, PaginatorButtonProps } from '../ui/Paginator.Button';
+import { render, screen } from '@testing-library/react';
+
+describe('Testing PaginatorButton', () => {
+  describe('Testing basic props', () => {
+    const firstPage = 1;
+    const props: PaginatorButtonProps = {
+      variant: 'first',
+      threshold: 1,
+      nextPage: 1,
+      currentPage: 10,
+    };
+
+    test('Is enabled', () => {
+      render(<PaginatorButton {...props} />);
+      expect(screen.getByRole('button')).toBeEnabled();
+    });
+    test('Is disabled', () => {
+      render(<PaginatorButton {...props} currentPage={firstPage} />);
+      expect(screen.getByRole('button')).toBeDisabled();
+    });
+  });
+});
