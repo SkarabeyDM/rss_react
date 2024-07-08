48262bed9e01d825540bee475a83056a9ce13442
SkarabeyDM <mechanoid0415@gmail.com>
2024-07-08 11:52:01 +0300

    feat: add api constants

---
+++ src/shared/const/const.ts
@@ -0,0 +1,21 @@
+export const API_KEY = import.meta.env.POKEMONTCG_API_KEY || "";
+export const BASE_ENDPOINT = "https://api.pokemontcg.io/v2/";
+
+export const API_KEY_HEADER = {
+  "X-Api-Key": API_KEY,
+};
+
+export enum EndpointVariants {
+  Cards = "cards",
+  Sets = "sets",
+  Types = "types",
+  Subtypes = "subtypes",
+  Supertypes = "supertypes",
+  Rarities = "rarities",
+}
+
+export enum Supertype {
+  Energy = "Energy",
+  Pokemon = "Pok\u00E9mon",
+  Trainer = "Trainer",
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
-import { defineConfig } from "vite";
+/// <reference types="vite/client" />
+
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "src": "/src",
-      "app": "/src/app",
-      "processes": "/src/processes",
-      "pages": "/src/pages",
-      "widgets": "/src/widgets",
-      "features": "/src/features",
-      "entities": "/src/entities",
-      "shared": "/src/shared",
+export default ({ mode }) => {
+  process.env = {
+    POKEMONTCG_API_KEY: process.env.VITE_API_KEY,
+    ...loadEnv(mode, process.cwd()),
+  };
+
+  return defineConfig({
+    plugins: [react()],
+    resolve: {
+      alias: {
+        src: "/src",
+        app: "/src/app",
+        processes: "/src/processes",
+        pages: "/src/pages",
+        widgets: "/src/widgets",
+        features: "/src/features",
+        entities: "/src/entities",
+        shared: "/src/shared",
+      },
     },
-  },
-});
+    define: {
+      "process.env": process.env
+    }
+  });
+};
