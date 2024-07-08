4ca04943f9ee9b4ca89515db2672b022a305d96b
SkarabeyDM <mechanoid0415@gmail.com>
2024-07-08 15:52:39 +0300

    feat: add api types

---
+++ src/shared/types/api.ts
@@ -0,0 +1,10 @@
+export type swapiPeople = {
+  name: string;
+  birth_year: string;
+  gender: string;
+  hair_color: string;
+  height: string;
+  mass: string;
+  skin_color: string;
+  eye_color: string;
+};
