9bf7cff97e749ad135d3ef07b6763f9d3dbd2fc7
SkarabeyDM <mechanoid0415@gmail.com>
2024-07-09 20:24:31 +0300

    config: add `eslint-config-airbnb`

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,38 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
+    'airbnb',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react-compiler"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: './tsconfig.app.json',
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ['react-refresh', 'react-compiler', '@typescript-eslint'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "react-compiler/react-compiler": "error",
+    'react-compiler/react-compiler': 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/prefer-stateless-function': 0,
+    'import/prefer-default-export': "off",
   },
 };
