f5cc175cbd2a3591095902c14dbe14a66a6eb7c5
SkarabeyDM <mechanoid0415@gmail.com>
2024-07-10 12:18:31 +0300

    refactor: change `App` component from class to function

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
 import './App.css';
 import { Search } from '@widgets/Search';
 import { ErrorBoundary } from '@shared/ui/ErrorBoundary';
 
-export class App extends React.Component {
-  render(): React.ReactNode {
-    return (
-      <main>
-        <ErrorBoundary fallback={<div>OOPS!</div>}>
-          <Search />
-        </ErrorBoundary>
-      </main>
-    );
-  }
+export function App() {
+  return (
+    <main>
+      <ErrorBoundary fallback={<div>OOPS!</div>}>
+        <Search />
+      </ErrorBoundary>
+    </main>
+  );
 }
