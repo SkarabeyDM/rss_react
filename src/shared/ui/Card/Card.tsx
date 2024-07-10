64c087eac37a922c09e8db4ed6b406938128aca6
SkarabeyDM <mechanoid0415@gmail.com>
2024-07-10 12:33:47 +0300

    refactor: change `Card` component from class to function

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,22 +1,16 @@
-import { Component } from 'react';
-import { SwapiPeople } from '@shared/types/api';
+import type { SwapiPeople } from '@shared/types/api';
 import style from './Card.module.scss';
 
 export type CardProps = Partial<SwapiPeople>;
 
-export class Card extends Component<CardProps> {
-  render() {
-    return (
-      <div className={style.card}>
-        {Object.entries(this.props).map(([key, value]) => {
-          return (
-            <div
-              className={style.card_line}
-              key={key}
-            >{`${key}: ${value}`}</div>
-          );
-        })}
-      </div>
-    );
-  }
+export function Card(props: CardProps) {
+  return (
+    <div className={style.card}>
+      {Object.entries(props).map(([key, value]) => {
+        return (
+          <div className={style.card_line} key={key}>{`${key}: ${value}`}</div>
+        );
+      })}
+    </div>
+  );
 }
