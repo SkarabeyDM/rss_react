481e2c88dce437817154fa481b1e909345392100
SkarabeyDM <mechanoid0415@gmail.com>
2024-07-31 15:39:08 +0300

    config: add testing packages
    "@sveltejs/vite-plugin-svelte",
    "@testing-library/svelte"

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,13 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelteTesting } from '@testing-library/svelte/vite';
 import viteConfig from './vite.config';
 
 export default defineConfig((configEnv) =>
   mergeConfig(
     viteConfig(configEnv),
     defineConfig({
+      plugins: [svelte(), svelteTesting()],
       test: {
         environment: 'jsdom',
         setupFiles: ['vitest-setup.ts'],
