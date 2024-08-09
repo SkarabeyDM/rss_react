ce8a6933b46eb443193762515f7c56f60444a2b1
SkarabeyDM <mechanoid0415@gmail.com>
2024-08-09 20:11:41 +0300

    feat: add `Wrapper` component
    this is necessary for switch theme

---
+++ src/shared/ui/Wrapper/Wrapper.tsx
@@ -0,0 +1,13 @@
+import { ThemeContext } from '@shared/themes';
+import classNames from 'classnames';
+import type { PropsWithChildren } from 'react';
+import { useContext } from 'react';
+
+export function Wrapper({ children }: PropsWithChildren) {
+  const { theme } = useContext(ThemeContext);
+  return (
+    <div className={classNames('app', theme)}>
+      <div className="wrapper">{children}</div>
+    </div>
+  );
+}
