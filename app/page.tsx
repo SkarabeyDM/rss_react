990b4178273cdf9e781af90b630ef18ca996151d
SkarabeyDM <mechanoid0415@gmail.com>
2024-08-11 19:55:52 +0300

    feat: add `app` folder

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable react-refresh/only-export-components */
+import type { Metadata } from 'next';
+
+export const metadata = { title: 'Star Wars Characters' } as Metadata;
+
+export default function MainPage() {
+  return <div>Main page</div>;
+}
