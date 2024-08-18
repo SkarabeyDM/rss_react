e362f25c9478f65ef499a46b31c70722b21a22c4
SkarabeyDM <mechanoid0415@gmail.com>
2024-08-19 01:32:40 +0300

    feat: add user list on MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,3 +1,17 @@
+import { UserCard } from '@entities/user/ui/UserCard'
+import { useAppSelector } from '@shared/store'
+import { selectUsers } from '@shared/store/formSlice'
+
 export function MainPage() {
-  return <h2>Choose a form</h2>
+  const users = useAppSelector(selectUsers)
+  return (
+    <div>
+      <h2>Choose a form</h2>
+      <section>
+        {users.map(userForm => (
+          <UserCard {...userForm} />
+        ))}
+      </section>
+    </div>
+  )
 }
